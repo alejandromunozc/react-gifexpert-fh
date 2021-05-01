9e37df2053ac02a97967af0836c2d6af614bb451
alejandromunozc <alejandromunozc@gmail.com>
2021-05-01 14:03:42 -0500

    Gif Expert App funcional

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GifExpertApp from './GifExpertApp.jsx'
+
+
+ReactDOM.render(
+
+    <GifExpertApp / > ,
+    document.getElementById('root')
+);
